b49aa15c44a9f3d6913920a6c91fd47bdbb84a0a
hideki.koike <hdkkik@gmail.com>
2020-10-22 01:17:23 +0900

    add unity project.

---
+++ Assets/TestGetArgs.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(InputField))]
+public class TestGetArgs : MonoBehaviour
+{
+    private InputField argText;
+    private string resultArgs = "None";
+
+    private void Reset()
+    {
+        if(!TryGetComponent(out argText))
+            gameObject.AddComponent<InputField>();
+    }
+
+    private void Awake()
+    {
+        argText = GetComponent<InputField>();
+    }
+
+    private void Start()
+    {
+        var args = Environment.GetCommandLineArgs();
+        resultArgs = $"Args count = {args.Length.ToString()}{Environment.NewLine}";
+
+        if (args.Length >= 100)
+        {
+            resultArgs += "Maximam = 100 args";
+            return;
+        }
+
+        foreach (var arg in args)
+        {
+            resultArgs += $"{arg}, {Environment.NewLine}";
+        }
+
+        argText.text += resultArgs;
+    }
+}
